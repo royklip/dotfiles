c6b63f366f3ccce2c55bcd3a0a356dfb27790edd
Roy Klip <royklip2@gmail.com>
2022-05-27 16:53:15 +0200

    Add install commands for apps, plugins and theme

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+# Update system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install wget, git and ZSH
+sudo apt -y install wget git zsh nano
+
+# Set default shell to ZSH
+sudo chsh -s $(which zsh)
+
+# Install Oh My ZSH
+sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install ZSH plugins and theme
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Copy config files to home folder
 cp ~/.dotfiles/runcom/.bash_profile ~
 cp ~/.dotfiles/runcom/.zshrc ~
+cp ~/.dotfiles/runcom/.p10k.zsh ~
 cp ~/.dotfiles/git/.gitconfig ~
