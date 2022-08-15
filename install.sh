4ce8e01e3aedea0f29c1879dcb0026d3fe8ed3ca
Roy Klip <royklip2@gmail.com>
2022-08-15 17:17:48 +0200

    Migrate the install code to one file

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-# Install packages
-sudo apt -y install wget zsh nano python3-pip python3.8-venv
+# Install git
+sudo apt -y install curl git wget zsh nano python3-pip python3.8-venv
 
 # Set default shell to ZSH
 sudo chsh -s $(which zsh)
@@ -13,13 +13,31 @@ sudo chsh -s $(which zsh)
 # Install Oh My ZSH
 sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Create ssh key
+ssh-keygen -t ed25519 -C "royklip2@gmail.com" -f "$HOME/.ssh/id_ed25519" -N ""
+ssh-add ~/.ssh/id_ed25519
+
+# Add ssh key to Github
+read -p "Enter a title for the key: " title
+read -p "Enter github personal access token: " githubpat
+pub=`cat ~/.ssh/id_ed25519.pub`
+curl -u "royklip:$githubpat" -X POST -d "{\"title\":\"$title\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+
 # Install ZSH plugins and theme
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Create projects directory
+mkdir ~/projects
+cd ~/projects
+
+# Clone the dotfiles repo
+git clone git@github.com:royklip/dotfiles.git
+cd dotfiles
+
 # Create symlinks to home folder
-ln -s ~/projects/dotfiles/runcom/.bash_profile ~/.bash_profile
+rm ~/.zshrc
 ln -s ~/projects/dotfiles/runcom/.zshrc ~/.zshrc
 ln -s ~/projects/dotfiles/runcom/.p10k.zsh ~/.p10k.zsh
 ln -s ~/projects/dotfiles/git/.gitconfig ~/.gitconfig
